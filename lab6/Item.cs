93938d036736e52da70939472ab0009c84edbe9f
Danil <70899358+Sonicman13@users.noreply.github.com>
2020-11-21 20:42:09 +0300

    lab12

--- lab6/Item.cs
+++ lab6/Item.cs
@@ -6,12 +6,16 @@ using System.Threading.Tasks;
 
 namespace lab6
 {
+    interface Add
+    {
+        void add();
+    }
     class Item
     {
-        private string code;
-        private string name;
-        private double price;
-        private int amount;
+        protected string code;
+        protected string name;
+        protected double price;
+        protected int amount;
         public void read()
         {
             Console.WriteLine("Введите название товара");
@@ -57,7 +61,7 @@ namespace lab6
             this.price = 0;
             this.amount = 0;
         }
-        public void display()
+        virtual public void display()
         {
             Console.WriteLine("Код товара:" + code);
             Console.WriteLine("Название товара:" + name);
@@ -94,6 +98,217 @@ namespace lab6
             {
                 return code;
             }
+        }
+    }
+
+    class Game : Item, Add, ICloneable
+    {
+        private int[] release = new int[3];
+        private string[] platforms = new string[10];
+        private string publisher;
+        public Game()
+        {
+            this.name = "-";
+            this.code = "-";
+            this.price = 0;
+            this.amount = 0;
+            this.release[0] = this.release[1] = this.release[2] = -1;
+            platforms[0] = "-";
+            publisher = "-";
+        }
+        public Game(string code, string name, double price, int amount, int[] release, string[] platforms, string publisher):base(code, name, price, amount)
+        {
+            int i;
+            for (i = 0; i < 3; i++)
+            {
+                this.release[i] = release[i];
+            }
+            for (i = 0; i < platforms.Length; i++)
+            {
+                this.platforms[i] = platforms[i];
+            }
+            this.publisher = publisher;
+        }
+        public void read(int d)
+        {
+            Console.WriteLine("Введите название товара");
+            name = Console.ReadLine();
+            Console.WriteLine("Введите код товара");
+            code = Console.ReadLine();
+            do
+            {
+                Console.WriteLine("Введите цену");
+                try
+                {
+                    price = Convert.ToDouble(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    price = -1;
+                }
+            } while (price < 0);
+            do
+            {
+                Console.WriteLine("Введите колличество товара");
+                try
+                {
+                    amount = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    amount = -1;
+                }
+            } while (amount < 0);
+            Console.WriteLine("Введите дату выхода(день, месяц(число), затем год(4 цифры), разделяя их нажатием Enter)");
+            release[0] = Convert.ToInt32(Console.ReadLine());
+            release[1] = Convert.ToInt32(Console.ReadLine());
+            release[2] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите платформы на которых доступна игра( чтобы закончить вводить платформы введите пустую строку)");
+            d = -1;
+            do
+            {
+                d++;
+                platforms[d] = Console.ReadLine();
+            } while (platforms[d] != "");
+            Console.WriteLine("Введите издателя");
+            publisher = Console.ReadLine();
+        }
+        public override void display()
+        {        
+            Console.WriteLine(this);
+        }
+        public override string ToString()
+        {
+            int d;
+            string s;
+            s = "Код товара: " + code + "\n";
+            s += "Название товара: " + name + "\n";
+            s += "Цена: " + price + "\n";
+            s += "Колличество: " + amount + "\n";
+            s += "Дата выхода: " + release[0] + "." + release[1] + "." + release[2] + "\n";
+            s += "Платформы: ";
+            for (d = 0; d < platforms.Length; d++)
+            {
+                s += platforms[d] + ", ";
+            }
+            s += "\n" + "Издатель: " + publisher;
+            return s;
+        }
+        public void add()
+        {
+            Console.WriteLine("Введите платформу на которой доступна игра");
+            platforms[platforms.Length] = Console.ReadLine();
+        }
+        public object Clone()
+        {
+            return new Game(code, name, price, amount, release, platforms, publisher);
+        }
+    }
+
+    class Platform : Item, Add
+    {
+        private string[] components = new string[10];
+        private string[] plusPlatforms = new string[10];
+        private string publisher;
+        public Platform()
+        {
+            this.name = "-";
+            this.code = "-";
+            this.price = 0;
+            this.amount = 0;
+            this.components[0] = "-";
+            this.plusPlatforms[0] = "-";
+            this.publisher = "-";
+        }
+        public Platform(string code, string name, double price, int amount, string[] components, string[] platforms, string publisher):base(code, name, price, amount)
+        {
+            int i;
+            for (i = 0; i < components.Length; i++)
+            {
+                this.components[i] = components[i];
+            }
+            for (i = 0; i < platforms.Length; i++)
+            {
+                this.plusPlatforms[i] = platforms[i];
+            }
+            this.publisher = publisher;
+        }
+        public void read(int d)
+        {
+            Console.WriteLine("Введите название товара");
+            name = Console.ReadLine();
+            Console.WriteLine("Введите код товара");
+            code = Console.ReadLine();
+            do
+            {
+                Console.WriteLine("Введите цену");
+                try
+                {
+                    price = Convert.ToDouble(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    price = -1;
+                }
+            } while (price < 0);
+            do
+            {
+                Console.WriteLine("Введите колличество товара");
+                try
+                {
+                    amount = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    amount = -1;
+                }
+            } while (amount < 0);
+            Console.WriteLine("Введите комплектацию( чтобы закончить вводить комплектацию введите пустую строку)");
+            d = -1;
+            do
+            {
+                d++;
+                components[d] = Console.ReadLine();
+            } while (components[d] != "");
+            Console.WriteLine("Введите платформы доступные по обратной совместимости( чтобы закончить вводить платформы введите пустую строку)");
+            d = -1;
+            do
+            {
+                d++;
+                plusPlatforms[d] = Console.ReadLine();
+            } while (plusPlatforms[d] != "");
+            Console.WriteLine("Введите издателя");
+            publisher = Console.ReadLine();
+        }
+        public override void display()
+        {
+            Console.WriteLine(this);
+        }
+        public override string ToString()
+        {
+            int d;
+            string s;
+            s = "Код товара: " + code + "\n";
+            s += "Название товара: " + name + "\n";
+            s += "Цена: " + price + "\n";
+            s += "Колличество: " + amount + "\n";
+            s += "Комплектация: ";
+            for (d = 0; d < components.Length; d++)
+            {
+                s += components[d] + ", ";
+            }
+            s += "\n" + "Платформы доступные по обратной совместимости: ";
+            for (d = 0; d < plusPlatforms.Length; d++)
+            {
+                s += plusPlatforms[d] + ", ";
+            }
+            s += "\n" + "Издатель: " + publisher;
+            return s;
+        }
+        public void add()
+        {
+            Console.WriteLine("Введите комплектующие");
+            components[components.Length] = Console.ReadLine();
         }
     }
 }
--- lab6/Store.cs
+++ lab6/Store.cs
@@ -12,7 +12,11 @@ namespace lab6
         private string adress;
         private int numberOfItems;
         private static int maxNumberOfItems = 10;
-        private Item[] item = new Item[10];
+        private int numberOfGames;
+        private int numberOfPlatforms;
+        private Item[] item = new Item[5];
+        private Game[] game = new Game[5];
+        private Platform[] platform = new Platform[5];
         public void read()
         {
             string f;
@@ -31,8 +35,30 @@ namespace lab6
                 Console.WriteLine("Добавить еще один товар?(1 - да, все остальные символы -нет)");
                 f = Console.ReadLine();
             }
+            numberOfGames = 0;
+            Console.WriteLine("Добавить игру?(1 - да, все остальные символы - нет)");
+            f = Console.ReadLine();
+            while (f == "1")
+            {
+                game[numberOfGames] = new Game();
+                game[numberOfGames].read();
+                numberOfGames++;
+                Console.WriteLine("Добавить еще одну игру?(1 - да, все остальные символы -нет)");
+                f = Console.ReadLine();
+            }
+            numberOfPlatforms = 0;
+            Console.WriteLine("Добавить консоль?(1 - да, все остальные символы - нет)");
+            f = Console.ReadLine();
+            while (f == "1")
+            {
+                platform[numberOfPlatforms] = new Platform();
+                platform[numberOfPlatforms].read();
+                numberOfPlatforms++;
+                Console.WriteLine("Добавить еще одну консоль?(1 - да, все остальные символы -нет)");
+                f = Console.ReadLine();
+            }
         }
-        public Store(string name, string adress, int numberOfItems, string[] itemName1, string[] itemCode1, double[] itemPrice1, int[] itemAmount1)
+        public Store(string name, string adress, int numberOfItems, Item[] item, int numberOfGames, Game[] game, int numberOfPlatforms, Platform[] platform)
         {
             int i;
             if (numberOfItems < Store.maxNumberOfItems)
@@ -42,7 +68,17 @@ namespace lab6
                 this.numberOfItems = numberOfItems;
                 for (i = 0; i < this.numberOfItems; i++)
                 {
-                    item[i] = new Item(itemCode1[i], itemName1[i], itemPrice1[i], itemAmount1[i]);
+                    this.item[i] = item[i];
+                }
+                this.numberOfGames = numberOfGames;
+                for (i = 0; i < this.numberOfGames; i++)
+                {
+                    this.game[i] = game[i];
+                }
+                this.numberOfPlatforms = numberOfPlatforms;
+                for (i = 0; i < this.numberOfPlatforms; i++)
+                {
+                    this.platform[i] = platform[i];
                 }
             }
         }
@@ -51,12 +87,16 @@ namespace lab6
             this.name = name;
             this.adress = "-";
             this.numberOfItems = 0;
+            this.numberOfGames = 0;
+            this.numberOfPlatforms = 0;
         }
         public Store()
         {
             this.name = "-";
             this.adress = "-";
             this.numberOfItems = 0;
+            this.numberOfGames = 0;
+            this.numberOfPlatforms = 0;
         }
         public void display()
         {
@@ -70,11 +110,22 @@ namespace lab6
                 Console.WriteLine("Товар " + (i + 1));
                 item[i].display();
             }
+            for (i = 0; i < numberOfGames; i++)
+            {
+                Console.WriteLine("Игра " + (i + 1));
+                game[i].display();
+            }
+            for (i = 0; i < numberOfPlatforms; i++)
+            {
+                Console.WriteLine("Консоль " + (i + 1));
+                platform[i].display();
+            }
         }
         public static Store operator ++ (Store store)
         {
             Store newStore = new Store();
             Exception a;
+            string f;
             int n;
             try
             {
@@ -89,9 +140,36 @@ namespace lab6
                     newStore.item[n] = new Item();
                     newStore.item[n] = store.item[n];
                 }
-                newStore.item[store.numberOfItems] = new Item();
-                newStore.item[store.numberOfItems].read();
-                newStore.numberOfItems = ++store.numberOfItems;
+                for (n = 0; n < store.numberOfGames; n++)
+                {
+                    newStore.game[n] = new Game();
+                    newStore.game[n] = store.game[n];
+                }
+                for (n = 0; n < store.numberOfPlatforms; n++)
+                {
+                    newStore.platform[n] = new Platform();
+                    newStore.platform[n] = store.platform[n];
+                }
+                Console.WriteLine("Добавить товар (1), игру (2) или консоль (3)");
+                f = Console.ReadLine();
+                if (f == "1")
+                {
+                    newStore.item[store.numberOfItems] = new Item();
+                    newStore.item[store.numberOfItems].read();
+                    newStore.numberOfItems = ++store.numberOfItems;
+                }
+                else if (f == "2")
+                {
+                    newStore.game[store.numberOfGames] = new Game();
+                    newStore.game[store.numberOfGames].read();
+                    newStore.numberOfGames = ++store.numberOfGames;
+                }
+                else
+                {
+                    newStore.platform[store.numberOfPlatforms] = new Platform();
+                    newStore.platform[store.numberOfPlatforms].read();
+                    newStore.numberOfPlatforms = ++store.numberOfPlatforms;
+                }
                 return newStore;
             }
             catch(Exception)
@@ -101,29 +179,86 @@ namespace lab6
         }
         public void priceChange(string code, double price)
         {
-            int i = 0;
+            int i = 0, f = 0;
             while (i < numberOfItems)
             {
                 if (item[i].Code == code)
                 {
                     item[i].Price = price;
                     i = numberOfItems;
+                    f = 1;
                 }
                 i++;
+            }
+            if (f == 0)
+            {
+                i = 0;
+                while (i < numberOfGames)
+                {
+                    if (game[i].Code == code)
+                    {
+                        game[i].Price = price;
+                        i = numberOfGames;
+                        f = 1;
+                    }
+                    i++;
+                }
+            }
+            if (f == 0)
+            {
+                i = 0;
+                while (i < numberOfPlatforms)
+                {
+                    if (platform[i].Code == code)
+                    {
+                        platform[i].Price = price;
+                        i = numberOfPlatforms;
+                        f = 1;
+                    }
+                    i++;
+                }
             }
         }
         public void amountChange(string code, int amountDifference)
         {
-            int i = 0;
+            int i = 0, f = 0;
             while (i < numberOfItems)
             {
                 if (item[i].Code == code)
                 {
                     item[i].Amount = item[i].Amount + amountDifference;
                     i = numberOfItems;
+                    f = 1;
                 }
                 i++;
             }
+            if (f == 0)
+            {
+                i = 0;
+                while (i < numberOfGames)
+                {
+                    if (game[i].Code == code)
+                    {
+                        game[i].Amount = game[i].Amount + amountDifference;
+                        i = numberOfGames;
+                        f = 1;
+                    }
+                    i++;
+                }
+            }
+            if (f == 0)
+            {
+                i = 0;
+                while (i < numberOfPlatforms)
+                {
+                    if (platform[i].Code == code)
+                    {
+                        platform[i].Amount = platform[i].Amount + amountDifference;
+                        i = numberOfPlatforms;
+                        f = 1;
+                    }
+                    i++;
+                }
         }
         public string Name
         {
@@ -153,6 +288,28 @@ namespace lab6
                 for (n = 0; n < store2.numberOfItems; n++)
                 {
                     newStore.item[i] = store2.item[n];
+                    i++;
+                }
+                newStore.numberOfGames = store1.numberOfGames + store2.numberOfGames;
+                for (n = 0; n < store1.numberOfGames; n++)
+                {
+                    newStore.game[n] = store1.game[n];
+                }
+                i = store1.numberOfGames;
+                for (n = 0; n < store2.numberOfGames; n++)
+                {
+                    newStore.game[i] = store2.game[n];
+                    i++;
+                }
+                newStore.numberOfPlatforms = store1.numberOfPlatforms + store2.numberOfPlatforms;
+                for (n = 0; n < store1.numberOfPlatforms; n++)
+                {
+                    newStore.platform[n] = store1.platform[n];
+                }
+                i = store1.numberOfPlatforms;
+                for (n = 0; n < store2.numberOfPlatforms; n++)
+                {
+                    newStore.platform[i] = store2.platform[n];
                     i++;
                 }
                 return newStore;
